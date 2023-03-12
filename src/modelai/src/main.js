5c330f5f6faeaca1e023f71d35c519ee1dcbf387
Syntaxa_R <xiaxi@e.ntu.edu.sg>
2023-03-12 16:37:45 +0800

    model-ai frontend

--- src/modelai/src/main.js
+++ src/modelai/src/main.js
@@ -1,11 +1,37 @@
 import { createApp } from 'vue'
+import {
+    create,
+    NButton,
+    NIcon,
+    NSpace,
+    NInput,
+    NBackTop,
+    NGradientText,
+    NPageHeader,
+    NLayout,
+    NLayoutHeader,
+    NLayoutContent,
+    NLayoutFooter,
+    NText,
+    NAvatar,
+    NMenu,
+    NImage,
+    NAlert
+} from 'naive-ui'
+
+import { VueMasonryPlugin } from 'vue-masonry'
+
+const naive = create({
+    components: [NButton, NIcon, NSpace, NInput, NBackTop, NGradientText, NPageHeader, NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter, NText, NAvatar, NMenu, NImage, NAlert],
+})
+
 import App from './App.vue'
-import router from './router'
 
 import './assets/main.css'
 
 const app = createApp(App)
+app.use(VueMasonryPlugin)
+app.use(naive)
 
-app.use(router)
 
 app.mount('#app')
